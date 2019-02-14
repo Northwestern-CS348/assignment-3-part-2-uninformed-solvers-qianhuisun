53c38ae28ecc877ed9af837f3fbc3a5e1a54d8d9
qianhuisun <qianhuisun2020@u.northwestern.edu>
2019-02-13 19:53:55 -0600

    SolverDFS first try

--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -19,7 +19,43 @@ class SolverDFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+
+        # if has reach winning condition
+        if self.currentState.state == self.victoryCondition:
+            return True
+
+        # enumerate all children state from current state, if unscaned and unvisited state found, return
+        movables = self.gm.getMovables()
+        for movable_index, movable in enumerate(movables):
+            # this children has already been scaned
+            if movable_index < self.currentState.nextChildToVisit:
+                continue
+            # this children is first scaned, but equal state may have been visited
+            self.gm.makeMove(movable)
+            childState = GameState(self.gm.getGameState(), self.currentState.depth + 1, movable)
+            childState.parent = self.currentState
+            self.currentState.children.append(childState)
+            self.currentState.nextChildToVisit = movable_index + 1
+            if childState in self.visited and self.visited[childState] == True:
+                self.gm.reverseMove(movable)
+                continue
+            else:
+                self.currentState = childState
+                self.visited[childState] = True
+                if childState.state == self.victoryCondition:
+                    return True
+                else:
+                    return False
+        # no new state found, thus back to parent State and solveOneStep
+        if self.currentState.parent:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            return self.solveOneStep()
+        else:
+            # TODO if DFS is finished and winning state is not reached
+            # TODO return False will cause Dead Loop in self.solve()
+            # TODO return True for now (Which should False)
+            return True
 
 
 class SolverBFS(UninformedSolver):
